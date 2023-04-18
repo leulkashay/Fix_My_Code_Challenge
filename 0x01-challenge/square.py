acc2ad4a3e289748256504dea556ec8fe2901e79
leulkashay <leulkashay2012@gmail.com>
2023-04-18 09:08:58 -0700

    new

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 class square():
-    
+    """ Square class. """    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """" Square class initialzed """  
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +16,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square instance """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """ Create Square instance """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
