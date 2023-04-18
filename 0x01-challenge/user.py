1a688b2bb6467ba648e8a66354fda8c23197c419
leulkashay <leulkashay2012@gmail.com>
2023-04-18 09:22:16 -0700

    update

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -8,19 +8,19 @@ class User():
 
     def __init__(self):
         """ Documentation """
-        self.__email = None
+        __email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+    
     @email.setter
     def email(self, value):
         """ Documentation """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
    
     
 if __name__ == "__main__":
